a741ff2410f1dfba9e52485e3ab0c37269f45be7
unknown <joshodhiambo5@gmail.com>
2023-03-28 22:16:54 +0300

    working on backend

--- chat.js
+++ chat.js
@@ -1,17 +1,21 @@
 import io from 'socket.io-client';
 
-const socket = io('http://192.168.0.29:8080');
+const socket = io('http://10.66.5.84:8080');
 
 socket.on('connect', () => {
   console.log('Socket.IO client connected');
   
   // Send a message to the server after the connection is established
   socket.emit('chat message', 'Hello, joshua!');
+  socket.emit('user location', 'Trying to get location!');
 });
 
 socket.on('chat message', (message) => {
   console.log(`Socket.IO message received: ${message}`);
 });
+socket.on('userLocation', (locationData) => {
+  console.log(`Received location data: ${locationData}`);
+})
 
 socket.on('disconnect', () => {
   console.log('Socket.IO client disconnected');
