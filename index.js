2695719f81115f4d28bb0471f61876d995a46d77
odhis101 <joshodhiambo5@gmail.com>
2023-04-01 11:10:57 +0300

    fixed the home page

--- index.js
+++ index.js
@@ -5,22 +5,29 @@ import { Server } from 'socket.io';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const ambulanceLocation = {};
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
   console.log('Socket.IO client connected');
+
 
   socket.on('chat message', (message) => {
     console.log(`Socket.IO message received: ${message}`);
 
     // Echo the message back to the client
-    socket.emit('chat message', message);
+    socket.emit('chat message', 'message');
   });
   socket.on('user location', (location) => {
-    console.log(`Socket.IO user  location received: ${location}`);
-
-    // Echo the message back to the client
+    console.log(`Socket.IO user location received: ${location}`);
     socket.emit('user location', location);
   });
+  socket.on('ambulance location', (location) => {
+    console.log(`Socket.IO ambulance location received: ${location}`);
+    ambulanceLocation[socket.id] = location;
+    console.log(ambulanceLocation);
+    io.emit('ambulance location updated', ambulanceLocation);
+
+ });
 
   socket.on('disconnect', () => {
     console.log('Socket.IO client disconnected');
